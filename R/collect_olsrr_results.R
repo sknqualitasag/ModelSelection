e434359a80bdee0f69d48dbe5f3a6ce1c2dea192
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2020-04-29 10:55:26 +0200

    Changed import of `%>%` to magrittr

--- R/collect_olsrr_results.R
+++ R/collect_olsrr_results.R
@@ -39,26 +39,23 @@
 #' @param pvec_sample vector of paths to data samples
 #' @param ps_response name of the response variable
 #' @param pvec_ignoring_columns vector of columns to ignore
-#' @param pvec_fixeffect_columns vector of columns as fixed effects
+#' @return tbl_result tibble with model selection results
+#'
+#' @importFrom dplyr `%>%`
+#'
 #' @export collect_ols_results
-collect_ols_results <- function(pvec_sample, ps_response, pvec_ignoring_columns, pvec_fixeffect_columns){
-
-  suppressPackageStartupMessages(if(! require("dplyr")) {
-    install.packages("dplyr", repos="https://stat.ethz.ch/CRAN/")
-    require("dplyr")
-  })
+collect_ols_results <- function(pvec_sample, ps_response, pvec_ignoring_columns){
 
   # read the first sample
-  tbl_first_sample <- readr::read_csv(file = pvec_sample[1], col_types = readr::cols())
+  tbl_first_sample <- read_sample(ps_sample_path = pvec_sample[1])
   # ignore some columns
-  tbl_first_sample <- tbl_first_sample %>% select(-c(pvec_ignoring_columns))
+  tbl_first_sample <- tbl_first_sample %>% dplyr::select(-c(pvec_ignoring_columns))
   # check that ps_response is in colnames(tbl_first_sample)
   if (!ps_response %in% colnames(tbl_first_sample))
     stop(" * ERROR: Cannot find response variable: ", ps_response, " in columnnames of first sample.")
-  # Distinguish between fix effects (as.factor) and covariables (still numeric)
-  for(fx in pvec_fixeffect_columns){
-    tbl_first_sample[[fx]] <- as.factor(tbl_first_sample[[fx]])
-  }
+  # check that no column is of type factor
+  if (any(sapply(colnames(tbl_first_sample), function(x) is.factor(tbl_first_sample[[x]]), USE.NAMES = FALSE)))
+    stop(" * ERROR in collect_ols_results: Cannot handle fixed effects - use function collect_stepAIC_results")
   # determine the predictors from column of first sample
   vec_pred <- setdiff(names(tbl_first_sample), ps_response)
   # the vector of column names of the result tibble
@@ -75,10 +72,14 @@ collect_ols_results <- function(pvec_sample, ps_response, pvec_ignoring_columns,
 
   # loop over the samples and do the model selection
   for (sidx in seq_along(pvec_sample)){
-    tbl_cur_sample <- readr::read_csv(file = pvec_sample[sidx], col_types = readr::cols())
+    tbl_cur_sample <- read_sample(ps_sample_path = pvec_sample[sidx])
     # check whether response variable is in colnames of tbl_cur_sample
     if (!ps_response %in% colnames(tbl_cur_sample))
       stop(" * ERROR: Cannot find response variable: ", ps_response, " in columnnames of current sample.")
+    # check that no column is of type factor
+    if (any(sapply(colnames(tbl_cur_sample), function(x) is.factor(tbl_cur_sample[[x]]), USE.NAMES = FALSE)))
+      stop(" * ERROR in collect_ols_results: Cannot handle fixed effects - use function collect_stepAIC_results")
+
     # fit full model to current sample data
     lm.full.cur <- lm(formula = formula.full, data = tbl_cur_sample)
     ols_result <- olsrr::ols_step_backward_aic(model = lm.full.cur)
